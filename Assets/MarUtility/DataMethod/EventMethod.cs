38da95e76cc8cb44cc07bdc69ca77edf233d361a
IceyFox006 <cgreenan@mail.bradley.edu>
2026-09-05 13:06:06 -0500

    PositionLerp

--- Assets/MarUtility/DataMethod/EventMethod.cs
+++ Assets/MarUtility/DataMethod/EventMethod.cs
@@ -1,7 +1,7 @@
 /*
  * Marlow Greenan
  * Created: 7/1/2026
- * Last Updated: 7/20/2026
+ * Last Updated: 09/05/2026 by Marlow Greenan
  * 
  * General methods used for buttons, animation events, etc.
  */
@@ -45,6 +45,13 @@ namespace MarUtility
             => cg.alpha = 1f;
         public void HideCanvasGroup(CanvasGroup cg)
             => cg.alpha = 0f;
+        #endregion
+
+        #region Move
+        public void PositionLerp(LerpPositionDataEndP lerpData)
+            => lerpData.BeginPositionLerp();
+        public void PositionLerp(LerpPositionDataEndT lerpData)
+            => lerpData.BeginPositionLerp();
         #endregion
     }
 }
--- Assets/MarUtility/Master/LerpMaster.cs
+++ Assets/MarUtility/Master/LerpMaster.cs
@@ -1,12 +1,14 @@
 /*
  * Marlow Greenan
  * Created: 6/26/2026
- * Last Updated: 7/19/2026
+ * Last Updated: 9/05/2026 by Marlow Greenan
  * 
  * Contains data for a lerp action.
  */
+using MarUtility.ExecutionManagement;
 using NaughtyAttributes;
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -23,22 +25,22 @@ namespace MarUtility
         END = 1 << 300,
     }
 
-    [System.Serializable]
+    [Serializable]
     public class LerpData
     {
         //VISUAL
         [SerializeField, MinValue(0.001f)]
-        private float _duration = 1;
+        protected float _duration = 1;
         [SerializeField, CurveRange(EColor.Green)]
         private AnimationCurve _curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
 
         //EVENTS
         [SerializeField]
-        private UnityEvent _onStart;
+        protected UnityEvent _onStart;
         [SerializeField]
-        private UnityEvent _onBody;
+        protected UnityEvent _onBody;
         [SerializeField]
-        private UnityEvent _onEnd;
+        protected UnityEvent _onEnd;
 
         #region GS
         public float Duration { get => _duration; set => _duration = value; }
@@ -47,6 +49,64 @@ namespace MarUtility
         public UnityEvent OnBody { get => _onBody; set => _onBody = value; }
         public UnityEvent OnEnd { get => _onEnd; set => _onEnd = value; }
         #endregion
+    }
+    //-----------------------------------------------------------------------------------------------------------------
+    [Serializable]
+    public class LerpPositionData : LerpData
+    {
+        [SerializeField, Required]
+        private Transform _movingTransform;
+
+        protected Vector3 lStart;
+        protected Vector3 lEnd;
+        private float lTime;
+        
+        public  virtual void BeginPositionLerp()
+            => SceneManager.INST.StartCoroutine(PositionLerpInterval());
+
+        //Lerps the _movingTransform's position.
+        public IEnumerator PositionLerpInterval()
+        {
+            lTime = 0;
+            _movingTransform.position = lStart;
+            _onStart.Invoke();
+
+            while (lTime < _duration)
+            {
+                _movingTransform.position += Vector3.Lerp(lStart, lEnd, lTime / _duration);
+                lTime += Time.deltaTime;
+                _onBody.Invoke();
+                yield return null;
+            }
+            _movingTransform.position = lEnd;
+            _onEnd.Invoke();
+        }
+    }
+    //-----------------------------------------------------------------------------------------------------------------
+    [Serializable]
+    public class LerpPositionDataEndT : LerpPositionData
+    {
+        [SerializeField]
+        private Transform _endTransform;
+
+        public override void BeginPositionLerp()
+        {
+            lEnd = _endTransform.position;
+            base.BeginPositionLerp();
+        }
+    }
+    //-----------------------------------------------------------------------------------------------------------------
+    [Serializable]
+    public class LerpPositionDataEndP : LerpPositionData
+    {
+        [SerializeField]
+        private Vector3 _endPosition;
+
+        public override void BeginPositionLerp()
+        {
+            lEnd = _endPosition;
+            base.BeginPositionLerp();
+        }
     }
 }
 
